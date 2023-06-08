b09d57696c6ef6402789a19a7c2aef0b881f9fb4
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-06-08 22:23:07 +0100

    added some funky corner borders

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my portfolio website.
+This is my portfolio website circa 2023.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image"
+import { FunkyBorders } from "@/components/FunkyBorders"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src={'/me.svg'}
-        alt='hmm... there should be a picture here...'
-        width={'500'}
-        height={'500'}
-      />
-      <div>{"Hold on, I've literally just started building this site today..."}</div>
+    <main className="flex min-h-screen min-h-xs min-w-screen bg-slate-900 justify-center p-5">
+      <FunkyBorders>
+        {'test'}
+      </FunkyBorders>
     </main>
   )
 }
