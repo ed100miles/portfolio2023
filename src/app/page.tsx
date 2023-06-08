84b2a1b3f8636057de42b8dc883a0cc4167c1030
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-06-08 15:50:19 +0100

    redirect link  to old site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image
         src={'/me.svg'}
-        alt='there should be a picture here...'
+        alt='hmm... there should be a picture here...'
         width={'500'}
         height={'500'}
       />
       <div>{"Hold on, I've literally just started building this site today..."}</div>
+      <div>{"Why not check out my last half built website:"}</div>
+      <a href={"https://portfolio2022-ruddy.vercel.app/"}>https://portfolio2022-ruddy.vercel.app/</a>
     </main>
   )
 }
