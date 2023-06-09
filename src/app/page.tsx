1b7d52833f792a3c232487c0ff9af78c2c5950c8
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-06-09 11:33:16 +0100

    better layout, breakpoints etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,20 @@ import { FunkyBorders } from "@/components/FunkyBorders"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-h-xs min-w-screen bg-vampire justify-center p-5 text-malachite font-mono text-sm md:text-base">
+    <main className="flex min-h-screen min-w-screen bg-vampire justify-center p-5 text-malachite font-mono text-sm lg:text-base">
       <FunkyBorders>
-        <div className="flex flex-col justify-evenly items-center h-full">
-        <div
-          className="w-2/3 text-center"
-        >{"Hold on, I've literally just started building this site today, and I've spent most of my time making those corner things..."}</div>
+        <div className="h-full w-full flex items-center justify-evenly flex-wrap sm:min-h-fit">
+          <div className="w-full h-[60vh] lg:h-2/3 lg:w-4/6 border border-malachite">
+
+          </div>
+          <div className="w-full h-[60vh] lg:h-2/3 lg:w-1/4">
+            <div className="pb-2 font-semibold text-lg">Directory</div>
+            <div className="h-fit w-full border-l-[1px] border-malachite pl-1">
+              <div>a</div>
+              <div>b</div>
+              <div>c</div>
+            </div>
+          </div>
         </div>
       </FunkyBorders>
     </main>
--- src/components/FunkyBorders.tsx
+++ src/components/FunkyBorders.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export const FunkyBorders = ({ children }: { children: ReactNode }) => (
-  <div className="flex flex-wrap w-full min-h-full md:max-w-6xl relative">
+  <div className="flex flex-wrap w-full h-[160vh] lg:h-[90vh] min-h-full lg:min-h-full lg:max-w-[80vw] relative">
     <div className="bg-gradient-to-br from-malachite from-7% via-islamGreen via-15% to-vampire to-50% rounded-tl-xl h-[30vmin] w-[30vmin] -z-1 relative">
       <div className="bg-vampire h-[95%] w-[95%] bottom-0 right-0 absolute rounded-md"></div>
     </div>
@@ -9,7 +9,7 @@ export const FunkyBorders = ({ children }: { children: ReactNode }) => (
       <div className="bg-vampire h-[95%] w-[95%] rounded-md"></div>
     </div>
     <div
-      className="absolute w-5/6 md:max-w-5xl h-5/6 z-10  flex items-center justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      className="absolute w-5/6 lg:max-w-5xl h-5/6 z-10 flex items-center justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       {children}
     </div>
   </div>
