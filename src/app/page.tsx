9c9b73a1d55482e6c4c60550a6e21771467d116f
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-06-09 16:10:43 +0100

    content component in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
 import { FunkyBorders } from "@/components/FunkyBorders"
+import { Content } from "./Content"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen bg-vampire justify-center p-5 text-malachite font-mono text-sm lg:text-base">
       <FunkyBorders>
-        <div className="h-full w-full flex items-center justify-evenly flex-wrap sm:min-h-fit">
-          <div className="w-full h-[60vh] lg:h-2/3 lg:w-4/6 border border-malachite">
-
-          </div>
-          <div className="w-full h-[60vh] lg:h-2/3 lg:w-1/4">
-            <div className="pb-2 font-semibold text-lg">Directory</div>
-            <div className="h-fit w-full border-l-[1px] border-malachite pl-1">
-              <div>a</div>
-              <div>b</div>
-              <div>c</div>
-            </div>
-          </div>
-        </div>
+        <Content/>
       </FunkyBorders>
     </main>
   )
