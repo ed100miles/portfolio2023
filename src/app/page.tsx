6afd14dcc93ae06b1a1c442b4634168648d2356a
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-06-09 10:04:35 +0100

    improved the borders and Matrixified the colour scheme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-import Image from "next/image"
 import { FunkyBorders } from "@/components/FunkyBorders"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-h-xs min-w-screen bg-slate-900 justify-center p-5">
+    <main className="flex min-h-screen min-h-xs min-w-screen bg-vampire justify-center p-5 text-malachite font-mono text-sm md:text-base">
       <FunkyBorders>
         <div className="flex flex-col justify-evenly items-center h-full">
-        <Image
-          src={'/me.svg'}
-          alt='hmm... there should be a picture here...'
-          width={'300'}
-          height={'300'}
-        />
         <div
           className="w-2/3 text-center"
-        >{"Hold on, I've literally just started building this site today, and I've spent most of my time making those corner things"}</div>
+        >{"Hold on, I've literally just started building this site today, and I've spent most of my time making those corner things..."}</div>
         </div>
       </FunkyBorders>
     </main>
