34ca480daa877efd569fe569dfb61259c33478d5
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-06-11 21:55:06 +0100

    icon tooltips, resume link

--- src/app/Icons.tsx
+++ src/app/Icons.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import * as Tooltip from '@radix-ui/react-tooltip';
+
 const MailIcon = () => (
   <svg
     viewBox="0 0 15 15"
@@ -39,18 +43,72 @@ const GithubIcon = () => (
   </svg>
 )
 
+const ResumeIcon = () => (
+  <svg
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3 2.5C3 2.22386 3.22386 2 3.5 2H9.08579C9.21839 2 9.34557 2.05268 9.43934 2.14645L11.8536 4.56066C11.9473 4.65443 12 4.78161 12 4.91421V12.5C12 12.7761 11.7761 13 11.5 13H3.5C3.22386 13 3 12.7761 3 12.5V2.5ZM3.5 1C2.67157 1 2 1.67157 2 2.5V12.5C2 13.3284 2.67157 14 3.5 14H11.5C12.3284 14 13 13.3284 13 12.5V4.91421C13 4.51639 12.842 4.13486 12.5607 3.85355L10.1464 1.43934C9.86514 1.15804 9.48361 1 9.08579 1H3.5ZM4.5 4C4.22386 4 4 4.22386 4 4.5C4 4.77614 4.22386 5 4.5 5H7.5C7.77614 5 8 4.77614 8 4.5C8 4.22386 7.77614 4 7.5 4H4.5ZM4.5 7C4.22386 7 4 7.22386 4 7.5C4 7.77614 4.22386 8 4.5 8H10.5C10.7761 8 11 7.77614 11 7.5C11 7.22386 10.7761 7 10.5 7H4.5ZM4.5 10C4.22386 10 4 10.2239 4 10.5C4 10.7761 4.22386 11 4.5 11H10.5C10.7761 11 11 10.7761 11 10.5C11 10.2239 10.7761 10 10.5 10H4.5Z"
+      fill="currentColor"
+      fillRule="evenodd"
+      clipRule="evenodd">
+    </path>
+  </svg>
+)
+
+
+const iconMap = [
+  {
+    icon: <ResumeIcon />,
+    href: "/Ed_Miles_Resume.pdf",
+    tooltip: 'Resume'
+  },
+  {
+    icon: <MailIcon />,
+    href: "mailto:edmilestech@gmail.com",
+    tooltip: 'Email'
+  },
+  {
+    icon: <LinkedInIcon />,
+    href: "https://www.linkedin.com/in/ed-miles-tech/",
+    tooltip: 'LinkedIn'
+  },
+  {
+    icon: <GithubIcon />,
+    href: "https://github.com/ed100miles",
+    tooltip: 'GitHub'
+  }
+]
+
 export const Icons = () => {
   return (
     <div className="w-full max-w-full flex space-x-3 md:space-x-10 pt-5 md:pt-10 pr-10">
-      <a className="w-4 md:w-5" href="mailto:edmilestech@gmail.com">
-        <MailIcon />
-      </a>
-      <a className="w-4 md:w-5" href="https://www.linkedin.com/in/ed-miles-tech/" target="_blank">
-        <LinkedInIcon />
-      </a>
-      <a className="w-4 md:w-5" href="https://github.com/ed100miles" target="_blank">
-        <GithubIcon />
-      </a>
+      {
+        iconMap.map((icon, idx) => (
+          <Tooltip.Provider
+            delayDuration={20}
+            key={idx}
+          >
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <a className="w-4 md:w-5" href={icon.href} target='_blank'>
+                  {icon.icon}
+                </a>
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <Tooltip.Content
+                  sideOffset={10}
+                  className='font-mono text-malachite rounded-md p-1 text-xs md:text-sm border border-malachite'
+                >
+                  {icon.tooltip}
+                  <Tooltip.Arrow className='fill-malachite' />
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        ))
+      }
     </div>
   )
 }
