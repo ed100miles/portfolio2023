dfb3f5a8aa17c54f35b7cd96c60ef57bcbcd8ac0
Ed Miles <edmiles@Eds-MacBook-Pro.local>
2023-06-21 14:32:27 +0100

    user letter, api hookup, start of showing found words

--- src/app/scrabble/page.tsx
+++ src/app/scrabble/page.tsx
@@ -1,19 +1,50 @@
 "use client"
-
-import { useState } from "react"
+import { KeyboardEvent, useEffect, useState } from "react"
+import { useDebounce } from "usehooks-ts"
 
 const emptyBoard = Object.fromEntries([...Array(225)].map((_, idx) => [idx, ""]))
-
-type Board = typeof emptyBoard
 
 
 export default function Scrabble() {
   const [boardState, setBoardState] = useState(emptyBoard)
   const [isWriteDown, setIsWriteDown] = useState(false)
   const [focusedSquare, setFocusedSquare] = useState(112)
+  const [letters, setLetters] = useState<string[]>(Array(7).fill(""))
+  const [boardFocused, setBoardFocused] = useState(true)
+  const [focusedLetter, setFocusedLetter] = useState(0)
+  const [foundWords, setFoundWords] = useState({})
+  const debouncedBoard = useDebounce(boardState, 1000)
+  const debouncedLetters = useDebounce(letters, 1000)
 
-  const Board = ({ boardState }: { boardState: Board }) => {
+  useEffect(() => {
+    const env = process.env.NODE_ENV
+    if (debouncedLetters.join("") !== "") {
+      let endpoint = ""
+      if (env === "development") {
+        endpoint = 'http://localhost:8000/'
+      }
+      else {
+        endpoint = "https://scrabble23-kdoaodt3pa-nw.a.run.app/"
+      }
+      fetch(endpoint,
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(
+            {
+              "board": debouncedBoard,
+              "letters": debouncedLetters.join("")
+            })
+        })
+        .then(response => response.json())
+        .then(response => setFoundWords(response.found))
+    }
+  }, [debouncedBoard, debouncedLetters])
 
+  const Board = () => {
     const handleNonAlphaKeys = (key: string, index: number) => {
       const newBoard = { ...boardState }
       switch (key) {
@@ -49,10 +80,10 @@ export default function Scrabble() {
       }
     }
 
-    const handleKeyUp = (e: any, index: number) => {
+    const handleKeyUp = (e: KeyboardEvent, index: number) => {
       const newBoard = { ...boardState }
       if (/^[A-Z]$/i.test(e.key)) { // 1 alpha character
-        newBoard[index] = e.key
+        newBoard[index] = e.key.toUpperCase()
         setBoardState(newBoard)
         if (isWriteDown) {
           setFocusedSquare(index + 15 < 225 ? index + 15 : index)
@@ -67,13 +98,18 @@ export default function Scrabble() {
     }
 
     return (
-      <div className="bg-teal-700 w-11/12 md:w-5/12 grid grid-cols-15 border rounded-sm">
+      <div
+        className="bg-teal-700 w-11/12 md:w-5/12 grid grid-cols-15 border rounded-sm"
+        onClick={() => setBoardFocused(true)}
+      >
         {
           Object.entries(boardState).map(([index, value]) => {
             return (
               <input
-                className={`rounded-sm aspect-square text-center text-sm md:text-lg text-black
-                  ${value === "" ? "bg-teal-700 border border-gray-400 " : "bg-orange-300 border-r border-b border-gray-700 drop-shadow-lg"}
+                className={` aspect-square text-center text-sm md:text-xl text-black
+                  ${value === ""
+                    ? "bg-teal-700 border border-gray-400 rounded-sm"
+                    : "bg-orange-300 border-r border-b border-gray-700 drop-shadow-lg rounded-md"}
                 `}
                 key={index}
                 value={value.toUpperCase()}
@@ -90,9 +126,68 @@ export default function Scrabble() {
     )
   }
 
-  return (
-    <main className="h-screen w-screen bg-sky-900 text-red-600 align-middle justify-center">
-      <Board boardState={boardState} />
-    </main>
+  const Letters = () => {
+    const handleLetterChange = (e: KeyboardEvent, index: number) => {
+      const newLetters = [...letters]
+      if (/^[A-Z]$/i.test(e.key)) { // 1 alpha character
+        newLetters[index] = e.key
+        setLetters(newLetters)
+        setFocusedLetter(focusedLetter + 1 >= 7 ? 0 : focusedLetter + 1)
+      }
+      else {
+        switch (e.key) {
+          case "Backspace": {
+            if (newLetters[index] == "") {
+              setFocusedLetter(focusedLetter > 0 ? focusedLetter - 1 : 6)
+            } else {
+              newLetters[index] = ""
+              setLetters(newLetters)
+            }
+            break
+          }
+          case "ArrowLeft": {
+            setFocusedLetter(focusedLetter > 0 ? focusedLetter - 1 : 6)
+            break
+          }
+          case "ArrowRight": {
+            setFocusedLetter(focusedLetter + 1 >= 7 ? 0 : focusedLetter + 1)
+            break
+          }
+        }
+      }
+    }
+
+    return (
+      <div
+        className="grid grid-cols-7 gap-1 w-5/6"
+        onClick={() => setBoardFocused(false)}
+      >
+        {letters.map((letter, index) => (
+          <input
+            key={index}
+            value={letter.toUpperCase()}
+            onChange={() => null}
+            onClick={() => setFocusedLetter(index)}
+            autoFocus={index == focusedLetter && !boardFocused}
+            onKeyDownCapture={(e) => handleLetterChange(e, index)}
+            className="aspect-square text-center text-sm md:text-xl text-black bg-orange-300 drop-shadow-lg rounded-lg"
+          >
+          </input>))}
+      </div>
+    )
+  }
+
+  const FoundWords = () => (
+    <div>
+      {Object.entries(foundWords).map((word) => <div key={word[0]}>{word[0]}</div>)}
+    </div >
   )
+
+return (
+  <main className="h-screen w-screen bg-sky-900 flex flex-col items-center justify-evenly">
+    <Board />
+    <Letters />
+    <FoundWords />
+  </main>
+)
 }
